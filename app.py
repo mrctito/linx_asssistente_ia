c4a0d2aa936f8c4a1d5d2bc0eaf1cd7c43a932a2
mrctito <mrctito.com>
2024-02-25 20:30:43 -0300

    ok

--- app.py
+++ app.py
@@ -5,22 +5,27 @@ from langchain.chains import LLMChain, ConversationalRetrievalChain
 import chainlit as cl
 from chat_service import GetConversationChain, monta_resposta_chat
 
+#chainlit run chat_interface.py
+
 
 @cl.on_chat_start
 async def on_chat_start():
-    msg = cl.Message(content=f"Processing ...", disable_feedback=True)
+    msg = cl.Message(content=f"Iniciando ...", disable_feedback=True)
     await msg.send()
-    chain = GetConversationChain()
+    chain = await GetConversationChain()
     cl.user_session.set("chain", chain)
+    msg = cl.Message(content=f"Pronto.", disable_feedback=True)
+    await msg.send()
 
 
 @cl.on_message
 async def main(message: cl.Message):
-    chain = cl.user_session.get("chain")  # type: ConversationalRetrievalChain
+    chain = cl.user_session.get("chain")
     cb = cl.AsyncLangchainCallbackHandler()
 
     #res = await chain.ainvoke(message.content, callbacks=[cb])
     res = await chain.ainvoke({"question": message.content}, callbacks=[cb])
+    reposta = monta_resposta_chat(res)
     answer = res["answer"]
     source_documents = res["source_documents"]  # type: List[Document]
 
@@ -31,7 +36,6 @@ async def main(message: cl.Message):
     if source_documents:
         for source_idx, source_doc in enumerate(source_documents):
             source_name = f"source_{source_idx}"
-            # Create the text element referenced in the message
             text_elements.append(
                 cl.Text(content=source_doc.page_content, name=source_name)
             )
