84f901c7a25ccd01bf77172f08cac1260c582086
mrctito <mrctito.com>
2024-02-25 20:38:05 -0300

    OK

--- chat_interface.py
+++ chat_interface.py
@@ -5,8 +5,7 @@ from langchain.chains import LLMChain, ConversationalRetrievalChain
 import chainlit as cl
 from chat_service import GetConversationChain, monta_resposta_chat
 
-#chainlit run chat_interface.py
-
+###chainlit run chat_interface.py
 
 @cl.on_chat_start
 async def on_chat_start():
@@ -22,28 +21,6 @@ async def on_chat_start():
 async def main(message: cl.Message):
     chain = cl.user_session.get("chain")
     cb = cl.AsyncLangchainCallbackHandler()
-
-    #res = await chain.ainvoke(message.content, callbacks=[cb])
     res = await chain.ainvoke({"question": message.content}, callbacks=[cb])
     reposta = monta_resposta_chat(res)
-    answer = res["answer"]
-    source_documents = res["source_documents"]  # type: List[Document]
-
-    resposta = monta_resposta_chat(res)
-
-    text_elements = []  # type: List[cl.Text]
-
-    if source_documents:
-        for source_idx, source_doc in enumerate(source_documents):
-            source_name = f"source_{source_idx}"
-            text_elements.append(
-                cl.Text(content=source_doc.page_content, name=source_name)
-            )
-        source_names = [text_el.name for text_el in text_elements]
-
-        if source_names:
-            answer += f"\nSources: {', '.join(source_names)}"
-        else:
-            answer += "\nNo sources found"
-
-    await cl.Message(content=answer, elements=text_elements).send()
+    await cl.Message(content=reposta).send()
